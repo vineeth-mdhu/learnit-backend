ebed5717a4fda34fa26fde12d8932f068ab192c7
Vineeth Madhu <vineethnbr13@gmail.com>
2023-06-17 10:32:55 +0530

    changes

--- routes/submit.js
+++ routes/submit.js
@@ -10,7 +10,7 @@ router.post('/', async (req, res) => {
     console.log(req)
     console.log(course_id,student_id,qs)
 
-    const map_level={"Easy":0.5,"Medium":1,"Difficult":2}
+    const map_level={"Easy":0.5,"Medium":1,"Hard":2}
     var comptencies = new Array(comptency_size).fill(0)
     var count = new Array(comptency_size).fill(0)
 
@@ -23,20 +23,23 @@ router.post('/', async (req, res) => {
     console.log(current_state)
 
     qs.forEach(element => {
-        console.log(element['correct'])
+        // console.log(element['correct'])
         if(element['correct']){
             console.log(element['skills'])
             for (var s in element['skills']) {
                 s=parseInt(s)
-                console.log(s)
+                // console.log(s)
                 comptencies[s]+=map_level[element['difficulty']]
+                // console.log(map_level[element['difficulty']])
+                console.log(comptencies)
                 count[s]+=1
                 
             }
-            console.log(comptencies)
+            
         }
     });
 
+    console.log(comptencies)
 
     for(let i=0;i<comptencies.length;i++){
         if(count[i]>0){
