f5d433a1014923a72af767c8c4d52852f109045f
Vineeth Madhu <vineethnbr13@gmail.com>
2023-06-17 09:24:44 +0530

    changes

--- routes/get-qs.js
+++ routes/get-qs.js
@@ -3,6 +3,24 @@ const { response } = require('express')
 const supabase = require('../utils/supabaseClient')
 
 router.post('/', async (req, res) => {
+
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+      
     const course_id=req.body.course_id
     console.log(req)
     const {data}=await supabase
@@ -10,17 +28,16 @@ router.post('/', async (req, res) => {
     .select('question_bank')
     .eq('course_id',course_id)
     console.log(data)
-    var size = 20
+    var size = 19
     var response= []
     var items= new Set()
-    while(size>0){
-        index=Math.floor(Math.random() * size);
-        if(!items.has(index)){
-        response.push(data[0].question_bank[index])
-        items.add(index)
+
+    shuffle(data[0].question_bank)
+    while(size>=0){
+        response.push(data[0].question_bank[size])
         size-=1
-        }
     }
+
     console.log(response)
     res.status(200).send(response)
     
